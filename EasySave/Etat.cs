48cd104122454806780e8d9e5c5814a60bf6e102
benvva <94329406+benvva@users.noreply.github.com>
2021-11-25 00:09:22 +0100

    Final Application Push
    Final application with logs/State/work/Language

---
+++ EasySave/Etat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet
+{
+    class Etat
+    {
+        public static string filePath = @"..\..\..\state.json";
+
+        public string Name { get; set; }
+        public string SourceFilePath { get; set; }
+        public string TargetFilePath { get; set; }
+        public string Time { get; set; }
+        public string State { get; set; }
+        public string TotalFilesToCopy { get; set; }
+        public string TotalFilesSize { get; set; }
+        public string NbFilesLeftToDo { get; set; }
+        public string Progression { get; set; }
+    }
+}
