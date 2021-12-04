3ab328c53f6504dac86cfcd480b240a1f5e0b24e
Penda <amelie.willems2@gmail.com>
2021-12-04 01:01:27 +0100

    change lang
    need to improve it later

--- V2-GUI/test1/test2/ViewModels/MainWindow.xaml.cs
+++ V2-GUI/test1/test2/ViewModels/MainWindow.xaml.cs
@@ -56,6 +56,110 @@ namespace test2
         }
 
         //tab3 Settings 
+
+        private void tab3ButtonFrench_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Button clickedButton = (Button)sender;
+            //clickedButton.Content = "...button clicked...";
+            clickedButton.IsEnabled = false;
+            tab3ButtonEnglish.IsEnabled = true;
+
+            tab1SaveWork.Header = "Ajouter un travail de sauvegarde";
+            tab2RunWork.Header = "Executer un travail de sauvegarde";
+            tab3Settings.Header = "Paramètres";
+            tab1TextBlockDescription.Text = "Le meilleur logiciel de gestion de sauvegarde que vous puissiez avoir !";
+            tab2TextBlockDescription.Text = "Le meilleur logiciel de gestion de sauvegarde que vous puissiez avoir !";
+            tab3TextBlockDescription.Text = "Le meilleur logiciel de gestion de sauvegarde que vous puissiez avoir !";
+            tab1LabelName.Content = "Nom du TS :";
+            tab1LabelSourcePath.Content = "Chemin source :";
+            tab1LabelTargetPath.Content = "Chemin cible :";
+            tab1LabelType.Content = "Type de TS :";
+            tab1SelectTypeFull.Content = "Complet";
+            tab1SelectTypeDifferential.Content = "Différentiel";
+            tab1LabelCrypted.Content = "Fichiers chiffrés :";
+            tab1CheckCrypted.Content = "Chiffrés";
+            tab1ButtonAdd.Content = "Ajouter";
+            tab2ButtonStartSequentialRun.Content = "Lancement séquentiel";
+            tab2TextBoxNumber.Text = "Nombre";
+            tab2ButtonStartSingleRun.Content = "Lancement unique";
+            tab2LabelProgessBar.Content = "Progression";
+            tab2ButtonPause.Content = "Pause";
+            tab2ButtonStop.Content = "Stop";
+            tab3ButtonFrench.Content = "Français";
+            tab3ButtonEnglish.Content = "Anglais";
+            tab3ButtonUserGuide.Content = "Ouvrir le guide utilisateur";
+            tab3ButtonOpenConfig.Content = "Ouvrir la configuration";
+            tab3ButtonOpenLogs.Content = "Ouvrir les logs";
+
+        }
+        private void tab3ButtonEnglish_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            //clickedButton.Content = "...button clicked...";
+            clickedButton.IsEnabled = false;
+            tab3ButtonFrench.IsEnabled = true;
+            /*
+            tab1SaveWork.Header = "Add Save Work";
+            tab2RunWork.Header = "Run Save Work";
+            tab3Settings.Header = "Settings";
+            tab1TextBlockDescription.Text = "The best backup management software you can have !";
+            tab2TextBlockDescription.Text = "The best backup management software you can have !";
+            tab3TextBlockDescription.Text = "The best backup management software you can have !";
+            tab1LabelName.Content = "Save work name :";
+            tab1LabelSourcePath.Content = "Source path :";
+            tab1LabelTargetPath.Content = "Target path :";
+            tab1LabelType.Content = "Save work type :";
+            tab1SelectTypeFull.Content = "Full";
+            tab1SelectTypeDifferential.Content = "Differential";
+            tab1LabelCrypted.Content = "Files encrypted :";
+            tab1CheckCrypted.Content = "Crypted";
+            tab1ButtonAdd.Content = "Add";
+            tab2ButtonStartSequentialRun.Content = "Start Sequential Run";
+            tab2TextBoxNumber.Text = "Number";
+            tab2ButtonStartSingleRun.Content = "Start Single Run";
+            tab2LabelProgessBar.Content = "Progress";
+            tab2ButtonPause.Content = "Pause";
+            tab2ButtonStop.Content = "Stop";
+            tab3ButtonFrench.Content = "French";
+            tab3ButtonEnglish.Content = "English";
+            tab3ButtonUserGuide.Content = "Open User Guide";
+            tab3ButtonOpenConfig.Content = "Open Config";
+            tab3ButtonOpenLogs.Content = "Open Logs";
+            */
+
+            Object[] contentToTranslate = {
+                tab1SaveWork.Header,
+                tab2RunWork.Header,
+                tab3Settings.Header,
+                tab1TextBlockDescription.Text,
+                tab2TextBlockDescription.Text,
+                tab3TextBlockDescription.Text,
+                tab1LabelName.Content,
+                tab1LabelSourcePath.Content,
+                tab1LabelTargetPath.Content,
+                tab1LabelType.Content,
+                tab1SelectTypeFull.Content,
+                tab1SelectTypeDifferential.Content,
+                tab1LabelCrypted.Content,
+                tab1CheckCrypted.Content,
+                tab1ButtonAdd.Content,
+                tab2ButtonStartSequentialRun.Content,
+                tab2TextBoxNumber.Text,
+                tab2ButtonStartSingleRun.Content,
+                tab2LabelProgessBar.Content,
+                tab2ButtonPause.Content,
+                tab2ButtonStop.Content,
+                tab3ButtonFrench.Content,
+                tab3ButtonEnglish.Content,
+                tab3ButtonUserGuide.Content,
+                tab3ButtonOpenConfig.Content,
+                tab3ButtonOpenLogs.Content
+            };
+
+            ChangeLang test = new ChangeLang();
+            test.changeLangFunction(contentToTranslate, test.englishTranslation);
+        }
         private void tab3ButtonUserGuide_Click(object sender, RoutedEventArgs e)
         {
             OpenProcess.OpenProcessFunction("notepad.exe", @"..\..\..\Files\UserGuide.txt");
@@ -73,10 +177,18 @@ namespace test2
 
         }
 
+        
+
         //button close
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
+
+
+
+        
+
+
     }
 }
