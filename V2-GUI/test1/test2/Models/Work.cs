b389752eeb517a3cdae7c780ca2de9f75ba16615
Penda <amelie.willems2@gmail.com>
2021-12-04 01:22:14 +0100

    add all the old files
    errors everywhere

---
+++ V2-GUI/test1/test2/Models/Work.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet
+{
+    class Work
+    {
+        public static string filePath = @"..\..\..\work.json";
+        public string name { get; set; }
+        public string repS { get; set; }
+        public string repC { get; set; }
+        public string type { get; set; }
+    }
+}
