72d8c8a5e7cfc975e6ad69ab3691c10adcf1db55
racim1403 <racim.amoura@viacesi.fr>
2021-12-06 02:49:19 +0100

    test 9999
    essaye de reglé merge probleme

--- V2-GUI/test1/test2/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
+++ V2-GUI/test1/test2/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
@@ -374,7 +374,7 @@ namespace test2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/test2;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/test2;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
