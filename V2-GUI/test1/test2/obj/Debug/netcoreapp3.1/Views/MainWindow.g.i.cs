03b2e1cdf51b4c17d236cba44af7efd07bccfb9d
racim1403 <racim.amoura@viacesi.fr>
2021-12-06 02:25:48 +0100

    app graph v2
    problème reglé

--- V2-GUI/test1/test2/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
+++ V2-GUI/test1/test2/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
@@ -18,6 +18,7 @@ using System.Windows.Controls.Primitives;
 using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.Integration;
 using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Markup;
@@ -373,7 +374,7 @@ namespace test2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/test2;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/test2;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
