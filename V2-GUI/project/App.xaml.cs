4f7e4a17d5f4c92583d26607c896837ca1e128e7
benvva <adel.belghait@viacesi.fr>
2021-12-16 23:53:22 +0100

    Application Mono Instance lazone

--- V2-GUI/project/App.xaml.cs
+++ V2-GUI/project/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,22 @@ namespace test2
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex _mutex = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string AppName = "test2";
+            bool OpenApp;
+
+            _mutex = new Mutex(true, AppName, out OpenApp);
+
+            if (!OpenApp)
+            {
+                //app is already running! Exiting the application
+                MessageBox.Show("App is already running my dude !");
+                Application.Current.Shutdown();
+            }
+            base.OnStartup(e);
+        }
     }
+
 }
